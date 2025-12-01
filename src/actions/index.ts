dd43fc1f5995fb066d33941311b10e944897d614
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2025-12-01 14:28:25 -0500

    ultima acualización

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,17 +1,30 @@
 import { ActionError, defineAction } from "astro:actions";
 import { Resend } from "resend";
+import { z } from "zod";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const server = {
     send: defineAction({
         accept: 'form',
-        handler: async () => {
+        input: z.object({
+            name: z.string(),
+            email: z.string().email(),
+            message: z.string()
+        }),
+
+        handler: async ({ name, email, message }) => {
             const { data, error } = await resend.emails.send({
-                from: 'Consultas <rtipiani@gmail.com>',
-                to: ['delivered@gmail.com'],
-                subject: 'Hello world',
-                html: '<strong>It works</strong>'
+                from: 'Consultas <np-reply@gmail.com>',
+                to: ['rtipiani@gmail.com'],
+                subject: `Nuevo mensaje de ${name}`,
+                html: `
+                    <h2>Nueva consulta desde la web</h2>
+                    <p><strong>Nombre:</strong> ${name}</p>
+                    <p><strong>Email:</strong> ${email}</p>
+                    <p><strong>Mensaje:</strong></p>
+                    <p>${message}</p>
+                `
             });
 
             if (error) {
