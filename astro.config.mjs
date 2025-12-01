603b3d4705c5073ff4fc599d43005dfddd91b254
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2025-12-01 16:09:56 -0500

    ultima acualización

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   adapter: vercel({}),
   integrations: [react()],
