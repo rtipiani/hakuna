54ac1c998b224dacec64be016b52925691ae3f33
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2025-12-01 10:39:33 -0500

    ultima acualización

--- src/components/Galeria.tsx
+++ src/components/Galeria.tsx
@@ -21,9 +21,16 @@ import img18 from "../assets/images/galeria/chanchamayo-4.png";
 import img19 from "../assets/images/galeria/chanchamayo-5.png";
 import img20 from "../assets/images/galeria/chanchamayo-6.png";
 
+import gal1 from "../assets/images/galeria/gal1.png";
+import gal2 from "../assets/images/galeria/gal2.png";
+import gal3 from "../assets/images/galeria/gal3.png";
+import gal4 from "../assets/images/galeria/gal4.png";
+import gal5 from "../assets/images/galeria/gal5.png";
+import gal6 from "../assets/images/galeria/gal6.png";
+
 export default function Gallery() {
     const images = [
-        img1, img2, img3, img4, img5, img6, img7,
+        gal1, gal2, gal3, gal4, gal5, gal6, img1, img2, img3, img4, img5, img6, img7,
         img8, img9, img10, img11, img12, img13, img14,
         img15, img16, img17, img18, img19, img20
     ];
